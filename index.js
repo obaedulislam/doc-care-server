a6d33074ece5932ff4261f2d094d57a770e54566
obayedul islam <obaed.cse@gmail.com>
2022-11-17 00:29:48 +0600

    Add data to MongoDB & Show it to the cliet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 4300;
 
 const app = express();
@@ -8,6 +9,29 @@ const app = express();
 //MiddleWare
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypkp6ia.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const appointmentCollection = client
+      .db("docCare")
+      .collection("appointmentOptions");
+
+    app.get("/appointmentoptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentCollection.find(query).toArray();
+      res.send(options);
+    });
+  } catch {}
+}
+run().catch(console.log());
 
 app.get("/", async (req, res) => {
   res.send("Doc Care Server is Running ");
